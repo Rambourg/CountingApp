ce14b65a613d50747132c4aa1232811a90103057
Lucas Rambourg <116593146+Rambourg@users.noreply.github.com>
2022-12-22 12:44:28 +0000

    Counting App
    first commit/ added interface and button with JS .

---
+++ index.js
@@ -0,0 +1,37 @@
+//examples
+//let count = 5
+//count = count = 1
+//console.log(count)
+
+//initialize the count as 0
+//listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the html to reflect the new count
+
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+
+
+function increment() {
+
+    count = count + 1
+    countEl.innerText = count
+    
+}
+
+function save() {
+    console.log(count)
+    
+}
+
+
+
+
+
+
+
+
+
+
